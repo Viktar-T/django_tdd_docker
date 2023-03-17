d3c0f2d7372229a1144fc4709bed7ce69f9ee2d4
Viktar Taustyka <vtaustyka@godeltech.com>
2023-03-17 17:59:01 +0100

    C4: CI-CD

--- app/movies/views.py
+++ app/movies/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import get_object_or_404, render
-from rest_framework import permissions, status
+from django.shortcuts import get_object_or_404
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
